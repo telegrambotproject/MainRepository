aca588baf410b89c40e4d23e27852330aca72a7c
Livbig <livbigovich@gmail.com>
2019-05-21 23:48:44 +0300

    IMDB keyword search

--- Main.py
+++ Main.py
@@ -39,3 +39,30 @@ def get_film(message):
         bot.send_photo(message.chat.id, photo=response['Poster'])
 
 bot.polling()
+
+#### STARTING THE IMDB KEYWORDS SEARCH
+lists_of_keywords = ['Action Hero', 'Tough Guy', 'Violence'] # Presumably got it from the text message
+
+IMDB_URL = 'https://www.imdb.com/search/keyword?keywords='
+additional_query = '&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=7846868c-8414-4178-8f43-9ad6b2ef0baf&pf_rd_r=Y51P6Q4PPA4PTDTZG5AP&pf_rd_s=center-1&pf_rd_t=15051&pf_rd_i=moka&ref_=kw_ref_key&sort=moviemeter,asc&mode=detail&page=1'
+
+
+def keywords_reformation(keyword):
+    parts = keyword.split(' ')
+    the_keyword = '-'.join(parts)
+    the_keyword = the_keyword.lower()
+    return the_keyword
+
+
+def search_by_keyword(keywords):
+    query = ''
+    the_keyword = ''
+    query = str(keywords_reformation(keywords[0]))
+    for i in range(1, len(keywords)):
+        the_keyword = keywords_reformation(keywords[i])
+        query = str(query + '%2C' + the_keyword)
+    return query
+
+
+Final_URL = str(IMDB_URL + search_by_keyword(lists_of_keywords) + additional_query)
+print(Final_URL)
