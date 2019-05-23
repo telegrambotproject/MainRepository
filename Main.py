61b6fb8c73fbbcab593bcd09ee8e81bb1ff2884c
ksnikiforov <kirnik77@gmail.com>
2019-05-23 23:45:43 +0300

    location test

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import telebot
 import requests
 
-
+location = ['']
 
 def search(loc, name):
     url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
@@ -9,36 +9,38 @@ def search(loc, name):
     params = {'key': key,
               'name': name,
               'radius': 1500,
-              'location': loc}
+              'location': loc,
+              'rankby': 'prominence'}
     r = requests.get(url=url, params=params)
-    print(r.json())
     return r.json()
 
 
 telebot.apihelper.proxy = {'https': 'https://188.152.158.252:8118'}
-bot = telebot.TeleBot('852946157:AAEv1Cg91DaHgGeEgbAKDRvDmm3EGY55nSI')
+bot = telebot.TeleBot('846385082:AAHf9ZM9ulaRR0b79g9oBC1mRqCXaor6GiA')
 
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Ты где НАХОДИШЬСЯ? Ара, я найду тебя! Метку поставь!')
+    bot.send_message(message.chat.id, 'скинь боту свою геолокацию')
 
 
 @bot.message_handler(content_types=['location'])
 def handle_location(message):
-    global location
-    location = f'{message.location.latitude}, {message.location.longitude}'
-    bot.send_message(message.chat.id, "Что ты хочешь найти?")
+    location[0] = f'{message.location.latitude}, {message.location.longitude}'
+    bot.send_message(message.chat.id, "Напиши пару мест через запятую")
     bot.register_next_step_handler(message, get_place)
 
 
 def get_place(message):
-    place = message.text
-    response = search(location, place)
-    print(place, response['results'][0])
-    lat = response['results'][0]['geometry']['location']['lat']
-    lon = response['results'][0]['geometry']['location']['lng']
-    bot.send_location(message.chat.id, lat, lon)
+    places = message.text.split(',')
+    for p in places:
+        response = search(location[0], p)
+        print(response['results'][0])
+        lat = response['results'][0]['geometry']['location']['lat']
+        lon = response['results'][0]['geometry']['location']['lng']
+        bot.send_message(message.chat.id, response['results'][0]['name'])
+        bot.send_location(message.chat.id, lat, lon)
+        location[0] = f'{lat}, {lon}'
 
 
 bot.polling()
