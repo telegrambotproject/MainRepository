ccfa40557160573d440bbba44b2b87f93a1fe503
ksnikiforov <kirnik77@gmail.com>
2019-05-31 17:57:48 +0300

    Added comments.

--- Main.py
+++ Main.py
@@ -14,24 +14,24 @@ with open('info/dialogflow.txt') as f:
 with open('info/key.txt') as f:
     key = f.read()
 
-try:
+try:  # Создаю поддельный SSL сертификат
     _create_unverified_https_context = ssl._create_unverified_context
 except AttributeError:
-    # Legacy Python that doesn't verify HTTPS certificates by default
     pass
 else:
-    # Handle target environment that doesn't support HTTPS verification
     ssl._create_default_https_context = _create_unverified_https_context
+
 
 proxy = {'https': 'https://188.152.158.252:8118'}
 
 
 proxy_handler = urlrequest.ProxyHandler(proxy)
 opener = urlrequest.build_opener(proxy_handler)
-urllib.request.install_opener(opener)
+urllib.request.install_opener(opener)  # Установка прокси для запроса фаилов в телеграме.
 
-telebot.apihelper.proxy = proxy
-API_TOKEN = '846385082:AAHf9ZM9ulaRR0b79g9oBC1mRqCXaor6GiA'
+
+telebot.apihelper.proxy = proxy  # Прокси для телеграм бота бота
+API_TOKEN = '852946157:AAEv1Cg91DaHgGeEgbAKDRvDmm3EGY55nSI'
 bot = telebot.TeleBot(API_TOKEN)
 
 
@@ -40,7 +40,10 @@ waypoints = []
 waypoint_id = []
 
 
-def search(loc, name, key):
+# Поиск мест рядом через google api
+# loc = "Широта, Долгота"
+# name = "Название места"
+def search(loc: str, name, key):
     url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json'
     params = {'key': key,
               'name': name,
@@ -51,7 +54,10 @@ def search(loc, name, key):
     return r.json()
 
 
-def route(loc, waypoints, waypoint_id, place_loc):
+# loc = "Широта, Долгота" (место начала)
+# waypoints = ["Широта, Долгота", ...] (указывать все места по порядку)
+# waypoint_id = [id места, ...] (Опционально, но нельзя указывать отдельно от параметра waypoints.)
+def route(loc, waypoints, waypoint_id):
     url = 'https://www.google.com/maps/dir/'
     params = {'api': '1',
               'origin': loc,
@@ -72,43 +78,44 @@ def start_message(message):
     bot.send_message(message.chat.id, 'Пришли геолокацию.')
 
 
-@bot.message_handler(content_types=['location'])
+@bot.message_handler(content_types=['location'])  # Беру локацию от пользователя
 def handle_location(message):
-    location[0] = f'{message.location.latitude}, {message.location.longitude}'
+    location[0] = f'{message.location.latitude}, {message.location.longitude}'  # задаю глобальный параметр location.
     bot.send_message(message.chat.id, "Напиши пару мест через запятую\n Например: Кино, Парк, Ресторан")
-    bot.register_next_step_handler(message, get_place)
+    bot.register_next_step_handler(message, get_places)
 
 
-def get_place(message):
+def get_places(message):  # Поиск множества мест
     places = message.text.split(',')
     print(places)
     for p in places:
-        response = search(location[0], p, key)
+        response = search(location[0], p, key)  # параметр location - глобальный.
         print(response['results'][0])
         lat = response['results'][0]['geometry']['location']['lat']
         lon = response['results'][0]['geometry']['location']['lng']
         bot.send_message(message.chat.id, response['results'][0]['name'])
         waypoints.append(f'{lat}, {lon}')
         waypoint_id.append(response['results'][0]['place_id'])
-    m = route(location[0], waypoints, waypoint_id, location[0])
+    m = route(location[0], waypoints, waypoint_id)
     print(m)
     bot.send_message(message.chat.id, m)
 
 
-@bot.message_handler(content_types=['voice'])
+@bot.message_handler(content_types=['voice'])  # Функция ловит все голосовые сообщения
 def handle_voice(message):
-    r = sr.Recognizer()
+    r = sr.Recognizer()  # нужно для библиотеки "speech_recognition"
     print(message.voice)
     file_info = bot.get_file(message.voice.file_id)
+    # рекветс через проксти, тк телеграм заблочен.
     file = urllib.request.urlopen(f'https://api.telegram.org/file/bot{API_TOKEN}/{file_info.file_path}')
-    with open('audio.ogg', 'wb') as audio:
+    with open('audio.ogg', 'wb') as audio:  # сохраняю аудио на компьютер.
         audio.write(file.read())
-        ogg = AudioSegment.from_ogg("audio.ogg")
+        ogg = AudioSegment.from_ogg("audio.ogg")  # Переконвертация в формат wav
         ogg.export("audio.wav", format="wav")
     with sr.AudioFile('audio.wav') as source:
         audio = r.record(source)
         try:
-            text = r.recognize_google_cloud(audio, credentials_json=credentials)
+            text = r.recognize_google_cloud(audio, credentials_json=credentials)  # запрос google speech
             request = apiai.ApiAI(dialogflow).text_request()
             request.lang = 'en'
             request.query = text
@@ -116,13 +123,13 @@ def handle_voice(message):
             responseJson = json.loads(request.getresponse().read().decode('utf-8'))
             print(responseJson)
             response = responseJson['result']['fulfillment']['speech']  # Разбираем JSON и вытаскиваем ответ
-            # Если есть ответ от бота - присылаем юзеру, если нет - бот его не понял
+            # Если есть ответ от бота - присылаем юзеру, если нет, то гугл не разобрал аудио.
             if response:
                 bot.send_message(message.chat.id, text=response)
             else:
-                bot.send_message(message.chat.id, text='Я Вас не совсем понял!')
+                bot.send_message(message.chat.id, text='I did not understand you')
         except sr.UnknownValueError:
-            bot.send_message(message.chat.id, 'Не понял')
+            bot.send_message(message.chat.id, 'UnknownValueError')
         except sr.RequestError as e:
             print(f"Could not request results from Google Speech Recognition service; {e}")
 
