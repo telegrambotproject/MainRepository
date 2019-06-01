1cb225cf1e3a1cef113e67bd83be8aaa024e731c
Livbig <livbigovich@gmail.com>
2019-06-01 20:15:57 +0300

    SEARCH OF MOVIES AND BUTTONS!))))

--- newMain.py
+++ newMain.py
@@ -1,9 +1,12 @@
 import telebot
 from urllib.request import urlopen
+import functions
+
+
 import requests
 
 
-telebot.apihelper.proxy = {'https': 'https://54.39.24.33:3128'}
+telebot.apihelper.proxy = {'https': 'https://153.92.5.186:8080'}
 bot = telebot.TeleBot('852946157:AAEv1Cg91DaHgGeEgbAKDRvDmm3EGY55nSI')
 
 
@@ -20,12 +23,21 @@ def start(message):
 @bot.message_handler(content_types=['text'])
 def first_chose(message):
     if message.text == 'Yes, please!':
-        markup = telebot.types.ReplyKeyboardRemove(selective=False)
-        bot.send_message(message.chat.id, "Let's get going then",
+        list_of_movies = functions.search_current_movies()
+        markup = telebot.types.ReplyKeyboardMarkup()
+        button_1 = telebot.types.KeyboardButton('1')
+        button_2 = telebot.types.KeyboardButton('2')
+        button_3 = telebot.types.KeyboardButton('3')
+        button_4 = telebot.types.KeyboardButton('4')
+        button_5 = telebot.types.KeyboardButton('5')
+        markup.add(button_1, button_2, button_3, button_4, button_5)
+        for i in range(len(list_of_movies)):
+            line = ''
+            for j in range(len(list_of_movies[i])):
+                line += str(list_of_movies[i][j])
+            bot.send_message(message.chat.id, line)
+        bot.send_message(message.chat.id, "Let's get going then! Which of these interest you the most?",
                          reply_markup=markup)
-        request = 'http://api.kinopoisk.cf/getTodayFilms'
-        response_body = urlopen(request).read()
-        bot.send_message(message.chat.id, response_body)
     elif message.text == 'Hell no!':
         markup = telebot.types.ReplyKeyboardRemove(selective=False)
         bot.send_message(message.chat.id, 'I am sorry to hear that',
