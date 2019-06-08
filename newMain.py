665d67c56fa22b1a0d613f71250ee6456c6f697a
ksnikiforov <kirnik77@gmail.com>
2019-06-09 01:27:52 +0300

    Got code from Travel Bot branch. (This is a bad solution, but manual merging is too hard and i don't get it)

--- newMain.py
+++ newMain.py
@@ -1,15 +1,29 @@
 import telebot
 import functions
+import urllib
 
 telebot.apihelper.proxy = {'https': 'https://95.216.119.75:3128'}
 bot = telebot.TeleBot('852946157:AAEv1Cg91DaHgGeEgbAKDRvDmm3EGY55nSI')
 
+
+location = ['']  # нужно переписать
+waypoints = []
+waypoint_id = []
 global d
 d = functions.load_obj('data')
 print(d)
 
 user_params = {'imdb_id': [], 'favourite_cinema': ''}  # Дефалтный пресет для нового пользователя
 remove_markup = telebot.types.ReplyKeyboardRemove()
+
+with open('info/g_key.txt') as f:  # Ключ для google api
+    g_key = f.read()
+
+
+proxy = {'https': 'https://188.152.158.252:8118'}
+
+functions.request_proxy(proxy)
+functions.fake_ssl()
 
 
 @bot.message_handler(commands=['start'])
@@ -263,6 +277,39 @@ def delete_user_info(message):
         bot.send_message(message.chat.id, 'Your info is deleted.', reply_markup=remove_markup)
     else:
         bot.send_message(message.chat.id, 'Good choice', reply_markup=remove_markup)
+
+
+@bot.message_handler(content_types=['location'])  # Беру локацию от пользователя
+def handle_location(message):
+    location[0] = f'{message.location.latitude}, {message.location.longitude}'  # задаю глобальный параметр location.
+    bot.send_message(message.chat.id, "Напиши пару мест через запятую\n Например: Кино, Парк, Ресторан")
+    bot.register_next_step_handler(message, get_places)
+
+
+def get_places(message):  # Поиск множества мест
+    places = message.text.split(',')
+    print(places)
+    for p in places:
+        response = functions.search(location[0], p, g_key)  # параметр location - глобальный.
+        print(response['results'][0])
+        lat = response['results'][0]['geometry']['location']['lat']
+        lon = response['results'][0]['geometry']['location']['lng']
+        bot.send_message(message.chat.id, response['results'][0]['name'])
+        waypoints.append(f'{lat}, {lon}')
+        waypoint_id.append(response['results'][0]['place_id'])
+    m = functions.route(location[0], waypoints, waypoint_id)
+    print(m)
+    bot.send_message(message.chat.id, m)
+
+
+@bot.message_handler(content_types=['voice'])  # Функция ловит все голосовые сообщения
+def handle_voice(message):
+    print(message.voice)
+    file_info = bot.get_file(message.voice.file_id)
+    # рекветс через проксти, тк телеграм заблочен.
+    file = urllib.request.urlopen(f'https://api.telegram.org/file/bot{API_TOKEN}/{file_info.file_path}')
+    response = functions.google_speech_request(file)
+    bot.send_message(message.chat.id, response)
 
 
 bot.polling()
